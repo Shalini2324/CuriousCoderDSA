19e7f4b1e6bd118f26b1a7ba745a0c2424d8dd57
Shalini2324 <shalinimohan024@gmail.com>
2025-11-23 01:35:16 +0530

    Create program5.java

---
+++ program5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class program5{
+    public static void main(String[] args){
+        System.out.println("Enter input");
+        Scanner scan=new Scanner(System.in);
+        int x=scan.nextInt();
+        int y=scan.nextInt();
+        for(int i=0;i<y;i++){
+            System.out.println(x);
+        }
+    }
+}
+        
