08535989e6614032176c27b24b78e3805e9b2975
Shalini2324 <shalinimohan024@gmail.com>
2025-11-26 21:55:31 +0530

    Create pattern8.java

---
+++ week2patterns/pattern8.java
@@ -0,0 +1,27 @@
+//Sample 1:
+
+class pattern8{
+    public static void main(String[]args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+//Sample 2:
+
+class pattern8{
+    public static void main(String[]args){
+        int n=6;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
