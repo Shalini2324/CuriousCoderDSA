48bc484bff3ad9e719132ec0e8b4c7c6aaca7018
Shalini2324 <shalinimohan024@gmail.com>
2025-12-05 23:21:10 +0530

    Create Number of divisors.java

---
+++ Week3/Number of divisors.java
@@ -0,0 +1,43 @@
+//https://www.geeksforgeeks.org/problems/number-of-divisors1631/1?
+class Solution {
+    static int countDivisors(int n) {
+        // code here
+        int count = 0;
+        
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            int k = n / i;
+            
+            if (n % i == 0) {
+                if (i % 3 == 0) {
+                    count++;
+                    // System.out.println(i);
+                }
+                if (k % 3 == 0 && k != i) {
+                    // System.out.println(k);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+// in online compiler
+class Number of divisors {
+    public static void main(String[]args){
+       
+        int n=6;
+        int count =0;
+        for(int i=1;i<=Math.sqrt(n);i++){
+            int k=n/i;
+            if(n%i==0){
+                if(i%3==0){
+                    count++;
+                }
+                if(k%3==0 && k!=1){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
