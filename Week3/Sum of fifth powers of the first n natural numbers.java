02f7859a97bd855996eae71d82db70bd58a793cf
Shalini2324 <shalinimohan024@gmail.com>
2025-12-06 17:52:13 +0530

    Create Sum of fifth powers of the first n natural numbers.java

---
+++ Week3/Sum of fifth powers of the first n natural numbers.java
@@ -0,0 +1,23 @@
+//https://www.geeksforgeeks.org/problems/sum-of-fifth-powers-of-the-first-n-natural-numbers3415/1
+class Solution {
+    long sumOfFifthPowers(long N) {
+        // code here
+        long sum=0;
+        for(long i=1;i<=N;i++){
+            sum=sum+(i*i*i*i*i);
+        }
+        return sum;
+    }
+}
+
+//another method
+
+class Solution {
+    long sumOfFifthPowers(long N) {
+        // code here
+         long sum = 0;
+        for(int i = 1; i <= N; i++){
+            sum += (long) Math.pow(i, 5);
+        }
+        return sum;
+    
