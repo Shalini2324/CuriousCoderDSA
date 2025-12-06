3c5877e6065c61c89f5e8530c42ab4e91dd1133b
Shalini2324 <shalinimohan024@gmail.com>
2025-12-06 17:45:36 +0530

    Create CountSquares.java

---
+++ Week3/CountSquares.java
@@ -0,0 +1,33 @@
+//https://www.geeksforgeeks.org/problems/count-squares3649/1
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        int sum = 0;
+    for (int i = 1; i <= Math.sqrt(n); i++) {
+        if (i * i < n) {
+            sum++;
+        } else {
+            break;
+        }
+    }
+    return sum;
+    }
+}
+
+//online compiler
+class Main {
+    public static void main(String[]args){
+       int n=9;
+       int sum = 0;
+       for (int i = 1; i <= Math.sqrt(n); i++) {
+           if (i * i < n) {
+               sum++;
+               
+           } else {
+               break;
+           }
+    }
+    System.out.println(sum);
+    }
+    
+}
