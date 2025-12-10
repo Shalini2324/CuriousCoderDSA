3927b323888060f4b6fbaf65adb34215f0d4ea68
Shalini2324 <shalinimohan024@gmail.com>
2025-12-10 16:56:14 +0530

    Create Sum of Array.java

---
+++ Week4/Sum of Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    int arraySum(int arr[]) {
+        // code here
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        return sum;
+    }
+}
+
+//online compiler
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+        int sum=0;
+        int[] arr={3,4,56,7,5};
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
