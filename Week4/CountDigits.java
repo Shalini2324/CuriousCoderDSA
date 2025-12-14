dffb435b8ad493ad7d3e5d991ff0d1cc2c6af90e
Shalini2324 <shalinimohan024@gmail.com>
2025-12-14 16:21:43 +0530

    Create CountDigits.java

---
+++ Week4/CountDigits.java
@@ -0,0 +1,13 @@
+class Main {
+
+static int countDigits(int n){
+    if(n==0){
+        return 0;
+    }
+    return 1 + countDigits(n/10);
+}
+
+    public static void main(String[] args) {
+        System.out.println(countDigits(53543));
+    }
+}
