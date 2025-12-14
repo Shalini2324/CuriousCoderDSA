9fae2d0334dd7ce310915ba5bd08513dd027e650
Shalini2324 <shalinimohan024@gmail.com>
2025-12-14 19:51:08 +0530

    Create SumOfN.java

---
+++ Week4/SumOfN.java
@@ -0,0 +1,12 @@
+class Main {
+// Sum of N natural numbers
+static int sumOfN(int n){
+    if(n==0){
+        return 0;
+    }
+    return n+ sumOfN(n-1);
+}
+    public static void main(String[] args) {
+        System.out.println(sumOfN(5));
+    }
+}
