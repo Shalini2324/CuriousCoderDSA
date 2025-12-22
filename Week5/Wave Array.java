137634e2729afa76dd4aa7c0d519a83ae4d48704
Shalini2324 <shalinimohan024@gmail.com>
2025-12-23 00:13:01 +0530

    Create Wave Array.java

---
+++ Week5/Wave Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortInWave(int arr[]) {
+        // code here
+        
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i += 2) {
+
+            // swap arr[i] and arr[i+1]
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
